7ada94a96bbf25b7c7129bf843d1a6558213e6de
fardad2105 <fardadyadegare@yahoo.com>
2024-08-16 01:14:02 +0330

    create fraud microservice and connect it with customer microservice

---
+++ fraud/src/main/java/com/fy/fraud/controller/FraudController.java
@@ -0,0 +1,23 @@
+package com.fy.fraud.controller;
+
+import com.fy.fraud.model.dto.FraudCheckResponse;
+import com.fy.fraud.services.FraudCheckService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("api/v1/fraud-check")
+@Slf4j
+public class FraudController {
+
+    private FraudCheckService fraudCheckService;
+
+    @GetMapping(path = "{customerId}")
+    public FraudCheckResponse isFraudster(@PathVariable("customerId") Long customerId) {
+        boolean isFraudulentCustomer =  fraudCheckService.isFraudulentCustomer(customerId);
+        log.info("fraud check request for customer {}", customerId);
+        return new FraudCheckResponse(isFraudulentCustomer);
+    }
+}
