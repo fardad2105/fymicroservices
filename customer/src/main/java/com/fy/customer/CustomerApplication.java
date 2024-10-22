3083a4d008cbb8c5e942f5e79dbbaeaa9602bdb9
fardad2105 <fardadyadegare@yahoo.com>
2024-10-23 01:38:09 +0330

    create eureka microservice for connection microservices and also add spec file for implement project on bamboo

--- customer/src/main/java/com/fy/customer/CustomerApplication.java
+++ customer/src/main/java/com/fy/customer/CustomerApplication.java
@@ -2,7 +2,6 @@ package com.fy.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 /**
  * generate main customer module
  * created by: fardad yadegar
